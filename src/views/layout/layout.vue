3326942562571d5c060455e0f5c93e0aaf818660
MrZhangXin <1021479292@qq.com>
2019-06-11 14:28:34 +0800

    Update layout.vue

--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,23 +1,25 @@
 <template>
-    <div >
-        <nav-bar></nav-bar>
-        <div class="flexBox">
-            <div class="left">
-                <sidebar></sidebar>
-            </div>
-            <div class="right">
-                <tags-view></tags-view>
-                <app-main></app-main>
-            </div>
-        </div>
-    </div>
+       <div class="layout">
+           <nav-bar></nav-bar>
+           <div class="flexBox">
+               <div class="left">
+                   <sidebar></sidebar>
+               </div>
+               <div class="right">
+                   <div class="tags">
+                       <tags-view></tags-view>
+                   </div>
+                   <div style="height: 42px;"></div>
+                   <app-main></app-main>
+               </div>
+           </div>
+       </div>
 </template>
 <script>
     import Sidebar from './sidebar';
     import NavBar from './navbar';
     import TagsView from './tagsview';
     import AppMain from './appmain';
-
 
     export default{
         data(){
@@ -37,19 +39,46 @@
         }
     }
 </script>
-<style>
+<style lang="less">
+    .layout{
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+    a{
+        text-decoration: none;
+    }
     .flexBox{
+        max-width: 1920px;
+        height: 100%;
         position: relative;
     }
-   .flexBox>.left{
-       position: absolute;
-       top: 0;
-       left: 0;
+
+    .flexBox>.left{
        width: 190px;
-       bottom: 0;
-       z-index: 99999;
-   }
-   .flexBox>.right{
-       padding-left: 190px;
-   }
+        position: absolute;
+        left: 0;
+        top:0;
+        bottom: 0;
+        z-index: 999999;
+    }
+    .flexBox>.right{
+        background-color: #F3F6FB;
+        padding-left: 190px;
+        height: 100%;
+        position: relative;
+    }
+    .tags{
+        position: absolute;
+        padding-left: 190px;
+        top: 0;
+        left: 0;
+        width: 100%;
+    }
+    .el-tag .el-icon-close{
+        color: #000000;
+    }
+    .el-tag-active .el-icon-close{
+        color: #ffffff;
+    }
 </style>
